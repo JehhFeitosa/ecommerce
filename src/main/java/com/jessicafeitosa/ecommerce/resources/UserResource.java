dce8d7a464ff30073f9539c0e14106b753d38a89
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-11-04 12:33:24 -0300

    update users

--- src/main/java/com/jessicafeitosa/ecommerce/resources/UserResource.java
+++ src/main/java/com/jessicafeitosa/ecommerce/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.jessicafeitosa.ecommerce.resources;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,9 @@ import com.jessicafeitosa.ecommerce.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@GetMapping
 	public ResponseEntity<User> findAll(){
 		User u = new User(1L, "Maria", "maria@gmail.com", "9999", "12345");
-		
 		return ResponseEntity.ok().body(u);
 	}
 	
