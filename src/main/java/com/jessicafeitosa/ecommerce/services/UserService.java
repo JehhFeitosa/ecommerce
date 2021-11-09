d00c3326f4c57a79219503b25469f1bd525bf0c0
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-11-09 08:56:41 -0300

    Service Layer, component registration

---
+++ src/main/java/com/jessicafeitosa/ecommerce/services/UserService.java
@@ -0,0 +1,27 @@
+package com.jessicafeitosa.ecommerce.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jessicafeitosa.ecommerce.entities.User;
+import com.jessicafeitosa.ecommerce.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		 Optional<User> obj = repository.findById(id);
+		 return obj.get();
+	}
+
+}
