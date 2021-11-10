9a46eec9f877c0f900dc8fe110eb5796e542a151
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-11-10 18:54:04 -0300

    Many-to-many association with extra attributes PART 2

--- src/main/java/com/jessicafeitosa/ecommerce/config/TestConfig.java
+++ src/main/java/com/jessicafeitosa/ecommerce/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.jessicafeitosa.ecommerce.entities.Category;
 import com.jessicafeitosa.ecommerce.entities.Order;
+import com.jessicafeitosa.ecommerce.entities.OrderItem;
 import com.jessicafeitosa.ecommerce.entities.Product;
 import com.jessicafeitosa.ecommerce.entities.User;
 import com.jessicafeitosa.ecommerce.entities.enums.OrderStatus;
 import com.jessicafeitosa.ecommerce.repositories.CategoryRepository;
+import com.jessicafeitosa.ecommerce.repositories.OrderItemRepository;
 import com.jessicafeitosa.ecommerce.repositories.OrderRepository;
 import com.jessicafeitosa.ecommerce.repositories.ProductRepository;
 import com.jessicafeitosa.ecommerce.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 
 	//Tudo que for implementado dentro desse método
@@ -73,6 +78,13 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 	
 
