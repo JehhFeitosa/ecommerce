c2d88a3f5b557c778b59aa6a6d4f3cd56ab452ed
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-11-09 22:36:24 -0300

    OrderStatus enum

--- src/main/java/com/jessicafeitosa/ecommerce/config/TestConfig.java
+++ src/main/java/com/jessicafeitosa/ecommerce/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.jessicafeitosa.ecommerce.entities.Order;
 import com.jessicafeitosa.ecommerce.entities.User;
+import com.jessicafeitosa.ecommerce.entities.enums.OrderStatus;
 import com.jessicafeitosa.ecommerce.repositories.OrderRepository;
 import com.jessicafeitosa.ecommerce.repositories.UserRepository;
 
@@ -31,9 +32,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria", "maria@gmail.com", "9888898988", "123456");
 		User u2 = new User(null, "João", "joao@gmail.com", "9888898988", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
 
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
