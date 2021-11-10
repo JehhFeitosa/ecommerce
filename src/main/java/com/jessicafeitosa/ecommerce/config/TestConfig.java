3cd875dd4c507772222239609309e21fcfb4abef
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-11-10 08:49:06 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/jessicafeitosa/ecommerce/config/TestConfig.java
+++ src/main/java/com/jessicafeitosa/ecommerce/config/TestConfig.java
@@ -53,6 +53,15 @@ public class TestConfig implements CommandLineRunner{
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		
 		User u1 = new User(null, "Maria", "maria@gmail.com", "9888898988", "123456");
 		User u2 = new User(null, "João", "joao@gmail.com", "9888898988", "123456");
