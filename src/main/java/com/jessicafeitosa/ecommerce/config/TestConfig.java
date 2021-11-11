4f3c85014a3ca9da367dab651bcc7205f8f3a238
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-11-11 08:36:49 -0300

    Payment, one-to-one association

--- src/main/java/com/jessicafeitosa/ecommerce/config/TestConfig.java
+++ src/main/java/com/jessicafeitosa/ecommerce/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.jessicafeitosa.ecommerce.entities.Category;
 import com.jessicafeitosa.ecommerce.entities.Order;
 import com.jessicafeitosa.ecommerce.entities.OrderItem;
+import com.jessicafeitosa.ecommerce.entities.Payment;
 import com.jessicafeitosa.ecommerce.entities.Product;
 import com.jessicafeitosa.ecommerce.entities.User;
 import com.jessicafeitosa.ecommerce.entities.enums.OrderStatus;
@@ -85,6 +86,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null,  Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 
